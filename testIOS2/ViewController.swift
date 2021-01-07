032c66095d2066762d8ace253478681898ab3d4c
MauricioZarate <maur.mx@gmail.com>
2021-01-07 15:04:52 -0600

    Initial Commit

---
+++ testIOS2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testIOS2
+//
+//  Created by Mauricio Zarate on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
